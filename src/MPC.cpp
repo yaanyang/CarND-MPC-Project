da26ee741ea05f31824925d39f142ca5bebd088a
Chris Yang <Chris.Yang@wdc.com>
2018-06-29 16:17:59 -0700

    commit

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -105,8 +105,8 @@ class FG_eval {
       AD<double> delta0 = vars[delta_start + t - 1];
       AD<double> a0 = vars[a_start + t - 1];
 
-      AD<double> f0 = coeffs[0] + coeffs[1] * x0;
-      AD<double> psides0 = CppAD::atan(coeffs[1]);
+      AD<double> f0 = coeffs[0] + coeffs[1] * x0 + coeffs[2] * CppAD::pow(x0, 2) + coeffs[3] * CppAD::pow(x0, 3);
+      AD<double> psides0 = CppAD::atan(coeffs[1] + 2 * coeffs[2] * x0 + 3 * coeffs[3] * CppAD::pow(x0, 2));
       
       // NOTE: The use of `AD<double>` and use of `CppAD`!
       // This is also CppAD can compute derivatives and pass
@@ -263,7 +263,7 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   results.push_back(solution.x[a_start]);
 
   // Predicted trajectories
-  for (unsigned i = 1; i < N - 1; ++i) {
+  for (unsigned i = 1; i < N; ++i) {
       results.push_back(solution.x[x_start + i]);
       results.push_back(solution.x[y_start + i]);
   }
